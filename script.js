b2e2521c2cf6df90dd45e79ea16535b3d87e4e94
Rushi <rushikeshgandhmal@gmail.com>
2022-11-20 20:45:26 +0530

    Again functionality done

--- script.js
+++ script.js
@@ -27,3 +27,16 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "Lost the Game";
   }
 });
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").textContent = "?";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector("number").style.width = "15rem";
+});
