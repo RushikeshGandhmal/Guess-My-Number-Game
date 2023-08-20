180058287bdf9eab7ba97e4b8478adb7b56571f7
Rushi <rushikeshgandhmal@gmail.com>
2023-08-21 00:19:23 +0530

    added logic

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+const inputElement = document.querySelector(".check");
+let score = 20;
+
+inputElement.addEventListener("click", () => {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No Number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too High!";
+      document.querySelector(".score").textContent = --score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too Low!";
+      document.querySelector(".score").textContent = --score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
